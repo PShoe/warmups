06e001ae4c97f29bf28f6f9959571260ffe135b7
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-07 10:51:37 +1000

    decode exercise

--- sept6.js
+++ sept6.js
@@ -1,22 +1,42 @@
 console.log('Sept 6 Warmup')
 
 
+var players = {
+  bob: [3,2,6,11,9,2,6,9,10];
+  jim: [5,12,9,22,13,7,16,10,11];
+  fish: [2,2,4,5,4,3,6,4,1];
+  par: [3,4,5,5,3,3,4,3,5];
+}
 
-var bob = [3,2,6,11,9,2,6,9,10];
-var jim = [5,12,9,22,13,7,16,10,11];
-var fish = [2,2,4,5,4,3,6,4,1];
-var par = [3,4,5,5,3,3,4,3,5];
+  var bob = [3,2,6,11,9,2,6,9,10];
+  var jim = [5,12,9,22,13,7,16,10,11];
+  var fish = [2,2,4,5,4,3,6,4,1];
+  var par = [3,4,5,5,3,3,4,3,5];
 
 
 var add = function(a, b) {
     return a + b;
 }
-var totalScores = function (arr) {
-    var scoreSum = arr.reduce(add, 0);
+var totalScores = function (player) {
+    var scoreSum = player.reduce(add, 0);
     return scoreSum;
 }
 
 /// ask for further explanation of REDUCE method
+
+
+// CLASS EXAMPLES
+var totalScores2 = function (arr) {
+  for (var i = 0; i < arr.length; i ++){
+      arr = arr + arr[i]
+  }
+}
+
+
+players.keys().forEach(function(player)){
+  console.log(totalScores(players[player]);
+)
+}
 
 console.log("Bob: " + totalScores(bob));
 console.log("Jim: " + totalScores(jim));
---
+++ sept7.js
@@ -0,0 +1,22 @@
+console.log('Sept 7 decode');
+
+var message = "FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV, WKH YDOLDQW QHYHU WDVWH RI GHDWK EXW RQFH.";
+var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var cipher = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+var alphaArr = alpha.split('');
+var cipherArr = cipher.split('');
+
+var shiftLetter = function(char) {
+var index = cipherArr.indexOf(char);
+if (char.match(/\W/g)) {
+    var decodeLetter = char[0];
+  } else {
+    var decodeLetter = alphaArr[index];
+  }
+  return decodeLetter;
+}
+
+var messageArr = message.split('');
+var decode = messageArr.map(shiftLetter);
+decode = decode.join();
+console.log(decode);
