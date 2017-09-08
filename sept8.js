f34824616befac3959ed7bf5b2fcb7d6db5421a5
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-08 10:38:54 +1000

    coin flip warmup

---
+++ sept8.js
@@ -0,0 +1,57 @@
+console.log("Sept 8, The Ultimate Life Decider ")
+
+  var tailsArr = [];
+  var headsArr = [];
+
+  while (tailsArr.length < 5 && headsArr.length < 5){
+
+    var coin = Math.round(Math.random())
+
+    if (coin === 1){
+      coin = 'heads';
+      headsArr.push(coin);
+    } else if (coin === 0) {
+      coin = 'tails';
+      tailsArr.push(coin);
+    }
+    }
+
+  var tossArr = tailsArr.concat(headsArr);
+  tossArr = tossArr.join(" ");
+
+  var tossWinner = function () {
+    if (tailsArr.length < headsArr.length) {
+
+      console.log("Tosses: " + tossArr + "\n" + "Heads wins!")
+    } else {
+      console.log("Tosses: " + tossArr + "\n" + "Tails wins!")
+    }
+  }
+
+
+console.log(tossWinner());
+
+
+console.log("Sept 8, The Ultimate Life Decider 2")
+
+  var headsCount = 0;
+  var tailsCount = 0;
+
+  while (headsCount < 5 && tailsCount < 5){
+    var coin = Math.round(Math.random())
+    if (coin === 1){
+      headsCount += 1
+    } else if (coin === 0) {
+      tailsCount += 1
+    }}
+
+
+  var tossWinner2 = function () {
+    if (tailsCount < headsCount) {
+      console.log(headsCount + " :Heads " + tailsCount + " :Tails" + "\n" + "Heads wins!")
+    } else {
+      console.log(headsCount + " :Heads " + tailsCount + " :Tails" + "\n" + "Tails wins!")
+    }
+  }
+
+  console.log(tossWinner2());
