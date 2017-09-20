39421dafd0783a807d642dac4b49da3fab7f6cdd
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-20 10:33:53 +1000

    polished warmup

--- sept20.rb
+++ sept20.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 add_another_name = ""
 orders = {}
+
 until add_another_name == "n" do
   puts "Name for order: "
   name = gets.chomp
@@ -14,7 +15,6 @@ until add_another_name == "n" do
     puts "#{ name } wants to order: "
     item = gets.chomp
     orders[name][:items].push(item)
-
     puts "Add another item to the order? (y/n)"
     add_another_item = gets.chomp
   end
@@ -27,6 +27,6 @@ until add_another_name == "n" do
 
 end
 
-all_orders = orders.each {|key, value| puts "#{ key } ordered #{ orders[key][:items].join(" & ") }" }
+all_orders = orders.each {|key, value| puts "#{ key } ordered #{ orders[key][:items].join(" & ") }." }
 
 binding.pry
