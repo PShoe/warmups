b017191d133d219d827e5cad0dbf8d3a635b4446
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-09-21 10:42:07 +1000

    fixed +=

--- sept21Classes.rb
+++ sept21Classes.rb
@@ -5,19 +5,19 @@ require 'benchmark'
 class Robot
   def initialize
     @count = 0
-    @beginning_time = Time.now
+    @creation_time = Time.now
     @name = make_name
   end
   def name
-    @count = @count + 1
+    @count += 1
     @name
   end
   def make_name
     @name = SecureRandom.hex(3).upcase
   end
   def reset
-    @count = @count + 1
-    @beginning_time_reset = Time.now
+    @count += 1
+    @boot_time = Time.now
     @name = SecureRandom.hex(3).upcase
   end
   def instruction_count
@@ -25,7 +25,7 @@ class Robot
   end
   def timers
     end_time = Time.now
-    puts "#{(end_time - @beginning_time)} since creation and #{(end_time - @beginning_time_reset)} since last reboot"
+    puts "#{(end_time - @creation_time)} since creation and #{(end_time - @boot_time)} since last reboot"
   end
 end
 
